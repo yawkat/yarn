48673931aff62c99662acd3e0b468febefcffa48
Jonas Konrad <me@yawk.at>
2015-05-18 16:13:40 +0200

    Compile to target/classes for now

--- maven/src/main/java/at/yawk/yarn/maven/YarnCompileMojo.java
+++ maven/src/main/java/at/yawk/yarn/maven/YarnCompileMojo.java
@@ -4,48 +4,58 @@ import at.yawk.yarn.bytecode.BytecodeContext;
 import at.yawk.yarn.compiler.instruction.BeanTreeWriter;
 import at.yawk.yarn.compiler.tree.BeanTree;
 import at.yawk.yarn.compiler.tree.BeanTreeCompiler;
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import javassist.ClassPool;
 import javassist.NotFoundException;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.compiler.AbstractCompilerMojo;
+import org.apache.maven.plugin.compiler.CompilationFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.jar.JarArchiver;
+import org.codehaus.plexus.compiler.util.scan.SimpleSourceInclusionScanner;
+import org.codehaus.plexus.compiler.util.scan.SourceInclusionScanner;
 
 /**
  * @author yawkat
  */
 @Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE)
-public class YarnCompileMojo extends AbstractMojo {
+public class YarnCompileMojo extends AbstractCompilerMojo {
     @Parameter(defaultValue = "${project}")
     MavenProject project;
 
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException, CompilationFailureException {
         try {
-            doExecute();
+            walkAndGenerate();
         } catch (IOException e) {
             throw new UncheckedIOException(e);
-        } catch (NotFoundException | DependencyResolutionRequiredException e) {
+        } catch (NotFoundException e) {
             throw new RuntimeException(e);
         }
+
+        super.execute();
     }
 
-    private void doExecute() throws IOException, NotFoundException, DependencyResolutionRequiredException {
+    private void walkAndGenerate() throws IOException, NotFoundException {
         BeanTreeCompiler compiler = new BeanTreeCompiler();
 
         // Collect all class names we know
         List<String> classNames = new ArrayList<>();
         classNames.addAll(collectClassNames(Paths.get(project.getBuild().getOutputDirectory())));
-        for (String cp : project.getCompileClasspathElements()) {
+        for (String cp : getClasspathElements()) {
             Path location = Paths.get(cp);
             if (Files.isRegularFile(location)) {
                 try (FileSystem fs = FileSystems.newFileSystem(location, null)) {
@@ -59,7 +69,7 @@ public class YarnCompileMojo extends AbstractMojo {
         // Build javassist classpath
         ClassPool pool = new ClassPool();
         pool.appendSystemPath();
-        for (String cp : project.getCompileClasspathElements()) {
+        for (String cp : getClasspathElements()) {
             pool.appendClassPath(cp);
         }
 
@@ -71,7 +81,17 @@ public class YarnCompileMojo extends AbstractMojo {
 
         // generate code
         BeanTree tree = compiler.finishTree();
-        BeanTreeWriter.write(tree, Paths.get("gen"));
+        Path generationFolder = getSourceRoot();
+        BeanTreeWriter.write(tree, generationFolder);
+    }
+
+    private Path getSourceRoot() {
+        return Paths.get(project.getBuild().getDirectory(), "generated-sources", "yarn");
+    }
+
+    private Path getClassRoot() {
+        // todo use generated-classes and register with jar plugin
+        return Paths.get(project.getBuild().getOutputDirectory());
     }
 
     @SuppressWarnings("Convert2streamapi")
@@ -96,5 +116,62 @@ public class YarnCompileMojo extends AbstractMojo {
             throw new UncheckedIOException(e);
         }
         return names;
+    }
+
+    /////// COMPILER ///////
+
+    @Override
+    protected SourceInclusionScanner getSourceInclusionScanner(int staleMillis) {
+        return new SimpleSourceInclusionScanner(Collections.singleton("**/*.java"), Collections.emptySet());
+    }
+
+    @Override
+    protected SourceInclusionScanner getSourceInclusionScanner(String inputFileEnding) {
+        assert inputFileEnding.equals(".java");
+        return getSourceInclusionScanner(0);
+    }
+
+    @Override
+    protected List<String> getClasspathElements() {
+        try {
+            return project.getCompileClasspathElements();
+        } catch (DependencyResolutionRequiredException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected List<String> getCompileSourceRoots() {
+        return Collections.singletonList(getSourceRoot().toString());
+    }
+
+    @Override
+    protected File getOutputDirectory() {
+        return getClassRoot().toFile();
+    }
+
+    @Override
+    protected String getSource() {
+        return "1.8";
+    }
+
+    @Override
+    protected String getTarget() {
+        return "1.8";
+    }
+
+    @Override
+    protected String getCompilerArgument() {
+        return "";
+    }
+
+    @Override
+    protected Map<String, String> getCompilerArguments() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    protected File getGeneratedSourcesDirectory() {
+        return null;
     }
 }
